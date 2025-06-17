24be2b4c47597d691b96b93da3367c9dcff73df0
Sujal Chitrakar <sujalchitrakar9851182571@gmail.com>
2025-06-17 17:48:13 +0545

    updated vite

--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.VITE_API_URL ||
+  (import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://restaurant-management-system-y7o4.onrender.com/api");
+
+console.log("Current API URL:", baseURL); // For debugging
+console.log("Current Mode:", import.meta.env.MODE); // For debugging
+
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3000/api"
-      : "https://restaurant-management-system-y7o4.onrender.com/api",
+  baseURL,
   withCredentials: true,
 });
 
